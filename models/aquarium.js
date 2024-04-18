93ac18e1fe235bad5f1763369b55e9baee2bc275
Deeann01 <deepika.annapureddy04@gmail.com>
2024-04-17 20:32:15 -0500

    added minor changes

--- models/aquarium.js
+++ models/aquarium.js
@@ -6,18 +6,20 @@ const aquariumSchema = new mongoose.Schema({
   aquarium_type: {
     type : String,
     required: [true, "aquarium_type is required"],
-    minlength : [1, "aquarium_type must be at least 1"],
-    maxlength : [19, "aquarium_type  must be below 19"]
+    minlength : [1, "aquarium_type name should be at least 1"],
+    maxlength : [22, "aquarium_type name should be below 19"]
 },
   size: {
     type : String,
-    required: [true, "size field needs to be entered"],
-    minsize : [2, "size must be at least 2"],
+    required: [true, "size field name needs to be entered"],
+    minlength : [1, "size name should be at least 1"],
+    maxlength : [10, "size name should be below 10"]
 },
   cost: {
     type : Number,
     required: [true, "Cost fied needs to be entered"],
-    mincost: [1, "Cost must be at least $1"]
+    min : [1, "aquarium Cost should be at least $1"],
+    max : [1000, "aquarium cost should be below 1000$"]
   }
 });
 
